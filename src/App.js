dd598c0c3ab95bf9ce91914b5044540f9f68b5db
William Brower <will@mywisewallet.com>
2018-11-06 15:15:09 -0500

    test

--- src/App.js
+++ src/App.js
@@ -10,16 +10,24 @@ class App extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
-  handleSubmit(event) {
+  handleSearch(event) {
     alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
+
+    fetch("https://u9b604czc3.execute-api.us-east-1.amazonaws.com/default/getcompanyinfo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({name: this.state.value})
+    }).then(response => response.json()).then(json => this.setState({current: json}));
   }
 
   componentWillMount() {
@@ -32,19 +40,32 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.current == NULL) {
+      return (<div className="App">
+        <form onSubmit={this.handleSearch}>
+          <label>
+            Select Company:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value=""></option>
+              {this.state.companies.map((name) => (<option key={name} value={name}>{name}</option>))}
+            </select>
+          </label>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>);
+    }
     return (<div className="App">
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSearch}>
         <label>
           Select Company:
           <select value={this.state.value} onChange={this.handleChange}>
             <option value=""></option>
-            {this.state.companies.map((name) => (
-              <option key={name} value={name}>{name}</option>
-            ))}
+            {this.state.companies.map((name) => (<option key={name} value={name}>{name}</option>))}
           </select>
         </label>
         <input type="submit" value="Submit"/>
       </form>
+      <h1>{this.state.current}</h1>
     </div>);
   }
 }
