eef5b4cf9dc775b8a7c164b6c486a0ee4b640f29
William Brower <will@mywisewallet.com>
2018-11-06 15:34:40 -0500

    a

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     console.log(event.target.value);
     this.setState({
       [event.target.name]: event.target.value
-    }, console.log(this.state.selection));
+    });
   }
 
   handleSearch(event) {
@@ -30,7 +30,7 @@ class App extends Component {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({name: this.state.value})
+      body: JSON.stringify({name: this.state.selection})
     }).then(response => response.json()).then(json => this.setState({current: json.name, currentEScore: json.eScore}));
   }
 
