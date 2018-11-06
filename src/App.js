00fb3ec14213b0a970158a11e4980bad58d78e65
William Brower <will@mywisewallet.com>
2018-11-06 16:28:13 -0500

    a

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({name: this.state.current, eScore: parseInt(this.state.currentEScore), gScore: parseInt(this.state.currentGScore), sScore: parseInt(this.state.currentSScore), transactionString: this.state.currentTransactionNames})
-    });//.then(this.setState({current: undefined, currentEScore: undefined, currentGScore: undefined, currentSScore: undefined, currentTransactionNames: undefined}));
+    }).then(this.setState({current: undefined, currentEScore: undefined, currentGScore: undefined, currentSScore: undefined, currentTransactionNames: undefined}));
   }
 
   componentWillMount() {
