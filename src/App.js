2b649e56d869192211ec70071c39dc800c63e813
William Brower <will@mywisewallet.com>
2018-11-06 15:28:31 -0500

    a

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   handleChange(event) {
     console.log(event.target.name);
-    console.log(event.arget.value);
+    console.log(event.target.value);
     this.setState({[event.target.name]: event.target.value});
   }
 
